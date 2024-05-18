f6476b57c12aef63f656fc5a5d61dcadc6149eb3
kuz4n22 <mishvkin@gmail.com>
2024-05-18 03:44:13 +0300

    refactor: user.views

--- user/views.py
+++ user/views.py
@@ -2,11 +2,14 @@ from rest_framework import viewsets
 from .models import CustomUser, Contract
 from .serializers import UserSerializer, ContractSerializer
 from rest_framework.permissions import IsAuthenticated
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 class UserViewSet(viewsets.ModelViewSet):
-    queryset = CustomUser.objects.filter(user_type='client')
+    queryset = CustomUser.objects.filter(is_staff=False)
     serializer_class = UserSerializer
     permission_classes = [IsAuthenticated]
+
 
 class ContractViewSet(viewsets.ModelViewSet):
     queryset = Contract.objects.all()
@@ -15,3 +18,14 @@ class ContractViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+
+
+## вывод: на двух стульях не усидеть
+@login_required
+def user_profile(request):
+    user = request.user
+    context = {
+        "user": user
+    }
+    return render(request, "user/profile.html", context)
