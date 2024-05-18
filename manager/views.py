c3488ebe917235bea62717f96334c91943c8f446
kuz4n22 <mishvkin@gmail.com>
2024-05-18 03:43:54 +0300

    refactor: manager.views

--- manager/views.py
+++ manager/views.py
@@ -1,12 +1,17 @@
+# from rest_framework import generics
 from rest_framework import viewsets
 from user.models import CustomUser
 from user.serializers import UserSerializer
 from rest_framework.permissions import IsAuthenticated
 
+# class RegisterView(generics.CreateAPIView):
+#     serializer_class = RegisterSerializer
+#     permission_classes = [AllowAny]
+    
 class ManagerUserViewSet(viewsets.ModelViewSet):
-    queryset = CustomUser.objects.filter(user_type='client')
+    queryset = CustomUser.objects.filter(is_staff=False)
     serializer_class = UserSerializer
     permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
-        serializer.save(user_type='client')
+        serializer.save()
