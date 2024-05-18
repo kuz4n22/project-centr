663421300115d2ba7232680a354290b7d44fbabb
kuz4n22 <mishvkin@gmail.com>
2024-05-18 03:42:01 +0300

    refactor: redesign urls

--- user/urls.py
+++ user/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet, ContractViewSet
+from . import views
 
 router = DefaultRouter()
-router.register(r'users', UserViewSet)
-router.register(r'contracts', ContractViewSet)
+router.register(r'users', views.UserViewSet)
+router.register(r'contracts', views.ContractViewSet)
 
 urlpatterns = [
+    path('profile/', views.user_profile, name='user-profile'),
     path('', include(router.urls)),
+
 ]
