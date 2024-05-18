b7b1a813c1032829f0628332e6f1fed2b308d8ab
kuz4n22 <mishvkin@gmail.com>
2024-05-18 03:39:49 +0300

    refactor: add stngs for jwt, auth

--- apartment_planning/settings.py
+++ apartment_planning/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os
 from pathlib import Path
+from datetime import timedelta
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -39,12 +41,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
     'djoser',
     'manager',
     'user',
+    # 'auth',
 ]
 
 AUTH_USER_MODEL = 'user.CustomUser'
+
 
 DJOSER = {
     'USER_ID_FIELD': 'phone_number',
@@ -61,11 +66,32 @@ DJOSER = {
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
+}
+
+#обработчик авторизации
+AUTHENTICATION_BACKENDS = (
+    'auth.auth_backends.PhoneBackend',
+)
+
+# LOGIN_URL = 'user-profile'
+
+# настройка токена для авторизации
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': True,
+    'ALGORITHM': 'HS256',
+    'SIGNING_KEY': SECRET_KEY,
+    'VERIFYING_KEY': None,
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
 }
 
 MIDDLEWARE = [
