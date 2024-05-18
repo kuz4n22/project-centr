cd565b32f63e68738f36acce6ce2a56d8b73c8d9
kuz4n22 <mishvkin@gmail.com>
2024-05-18 03:43:41 +0300

    refactor: user.serializers

--- user/serializers.py
+++ user/serializers.py
@@ -4,11 +4,11 @@ from .models import CustomUser, Contract
 class ContractSerializer(serializers.ModelSerializer):
     class Meta:
         model = Contract
-        fields = ['id', 'contract_number', 'service_type', 'contract_date', 'completion_date', 'status']
+        fields = ['contract_number', 'service_type', 'contract_date', 'completion_date', 'status']
 
 class UserSerializer(serializers.ModelSerializer):
     contracts = ContractSerializer(many=True, read_only=True)
 
     class Meta:
         model = CustomUser
-        fields = ['id', 'phone_number', 'first_name', 'last_name', 'email', 'user_type', 'contracts']
+        fields = ['phone_number', 'first_name', 'last_name', 'email', 'contracts']
