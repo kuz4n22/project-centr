20a535bc385f5637d948d347827f7ce574847d2a
kuz4n22 <mishvkin@gmail.com>
2024-05-30 10:39:49 +0300

    refactor: comment out useless code (for Rest)

--- manager/views.py
+++ manager/views.py
@@ -5,8 +5,8 @@ from custom_auth.decorators import manager_required
 @login_required
 @manager_required
 def manager_dashboard(request):
-    manager = request.user
-    context = {
-        "manager": manager
-    }
-    return render(request, 'manager/dashboard.html', context)
+    # manager = request.user
+    # context = {
+    #     "manager": manager
+    # }
+    return render(request, 'manager/dashboard.html')
