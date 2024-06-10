c91cacac396cac56858ad531addf50015f159753
Genrikh Antipov <genrikhantipov@gmail.com>
2024-06-10 03:37:31 +0300

    feat: add area article for calcator

--- static/js/calculator.js
+++ static/js/calculator.js
@@ -1,15 +1,11 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const areaInput = document.getElementById('area');
-  const areaValue = document.getElementById('area-value');
-  const summaryArea = document.getElementById('summary-area');
-  const costElement = document.getElementById('cost');
-  let baseCost = 80000;
+// area animation
+const rangeSlider = document.getElementById('rangeSlider');
+const rangeValue = document.getElementById('rangeValue');
 
-areaInput.addEventListener('input', function () {
-  const area = areaInput.value;
-  areaValue.textContent = area + ' м²';
-  summaryArea.textContent = area + ' м²';
-  updateCost();
-});
+rangeSlider.addEventListener('input', () => {
+  const value = rangeSlider.value;
+  rangeValue.textContent = `${value} м²`;
 
+  const percentage = ((value - 40) / (200 - 40)) * 100;
+  rangeSlider.style.background = `linear-gradient(to right, #FFF ${percentage}%, rgba(255, 255, 255, 0.4) ${percentage}%)`;
 });
