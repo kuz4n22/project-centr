66bfc3dbcd09741cc0679318cfdfa615607d8801
kuz4n22 <mishvkin@gmail.com>
2024-06-15 16:19:39 +0300

    fix: delete contects obkect: user_contracts, add contects object: user_contract for auth user

--- user/context_processors.py
+++ user/context_processors.py
@@ -1,13 +1,23 @@
 
 #передача данныех контракта, для авторизированного юзера
 def user_contracts(request):
+    # '''
+    # Если юзер авторизирован, то в шаблон передается список "user_contracts",
+    # который содержит все контракты юзера, отсортированные по дате заключения
+    # контракта
+    # '''
+    # if request.user.is_authenticated:
+    #     return {
+    #         'user_contracts': request.user.contracts.all().order_by('-contract_date')
+    #     }
+    # return {}
     '''
-    Если юзер зарегестрирован, то в шаблон передается список "user_contracts",
-    который содержит все контракты юзера, отсортированные по дате заключения
-    контракта
+    Если юзер авторизирован, то в шаблон передается объект "user_contract",
+    который имеет параметры, соответствующие параметрам модели контракт
     '''
     if request.user.is_authenticated:
         return {
-            'user_contracts': request.user.contracts.all().order_by('-contract_date')
+            'user_contract': request.user.contracts.last()
         }
     return {}
+    
