ffab4c75201a3adbf2d021c8faa53deba40cc10b
Genrikh Antipov <genrikhantipov@gmail.com>
2024-06-19 13:54:20 +0300

    feat: work with dasboard btns

---
+++ static/js/stages.js
@@ -0,0 +1,71 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const finishBtns = document.getElementsByClassName('client-finish-btn');
+    Array.from(finishBtns).forEach(btn => {
+        btn.addEventListener('click', function () {
+
+            const clientId = btn.dataset.clientId;
+            const url = `/manager/complete_project/${clientId}/`;
+            
+            fetch(url, {
+                method: 'GET', 
+                headers: {
+                    'X-Requested-With': 'XMLHttpRequest', 
+                    'X-CSRFToken': '{{ csrf_token }}' 
+                },
+            })
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                console.log(data);  
+                alert(data.message);  
+            })
+            .catch(error => {
+                console.error('Error during fetch operation:', error);
+                alert('Произошла ошибка при завершении проекта');
+            });
+        });
+    });
+
+    const notifiBtns = document.getElementsByClassName('client-notifi-btn');
+    Array.from(notifiBtns).forEach(btn => {
+        btn.addEventListener('click', function () {
+
+            const clientId = btn.dataset.clientId;
+            const url = `/manager/notify_next_phase/${clientId}/`;
+
+            fetch(url, {
+                method: 'GET', 
+                headers: {
+                    'X-Requested-With': 'XMLHttpRequest', 
+                    'X-CSRFToken': '{{ csrf_token }}' 
+                },
+            })
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                console.log(data);  
+                alert(data.message);  
+            })
+            .catch(error => {
+                console.error('Error during fetch operation:', error);
+                alert('Произошла ошибка при завершении проекта');
+            });
+
+
+        });
+    });
+
+    const newPassBtns = document.getElementsByClassName('client-new-pass-btn');
+    Array.from(newPassBtns).forEach(btn => {
+        btn.addEventListener('click', function () {
+
+            const clientId = btn.dataset.clientId;
+
+
+
+        });
+    });
+});
+
