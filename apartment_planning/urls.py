2f21d019874627a8a627e8bb155fc874cae43023
Genrikh Antipov <genrikhantipov@gamil.com>
2024-06-20 16:58:20 +0300

    add changes of submodule

--- apartment_planning/urls.py
+++ apartment_planning/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path('', include('custom_auth.urls')),
     path('', include('user.urls')),
     path('manager/', include('manager.urls')),
-    path('__reload__/', include('django_browser_reload.urls')),
 ]
 
 handler404 = 'user.views.custom_404'
