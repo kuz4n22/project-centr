7510fe55753d5aff0163b3350577ca6a92332e5a
Genrikh Antipov <genrikhantipov@gmail.com>
2024-06-29 05:18:45 +0300

    refactor: add context processor to detect current page url

--- apartment_planning/settings.py
+++ apartment_planning/settings.py
@@ -76,7 +76,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'user.context_processors.user_contracts'
+                'user.context_processors.user_contracts',
+                'user.context_processors.current_url'
             ],
         },
     },
