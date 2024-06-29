480599d6b4a7967d8b6f7df01f60e0c0382686ea
Genrikh Antipov <genrikhantipov@gmail.com>
2024-06-30 02:31:28 +0300

    refactor: finish form responsive and mobile versions

--- static/js/checkbox.js
+++ static/js/checkbox.js
@@ -2,3 +2,8 @@ document.getElementById('customCheckbox').addEventListener('change', function() 
   const submitButton = document.getElementById('submitButton');
   submitButton.disabled = !this.checked;
 });
+
+document.getElementById('customCheckboxMobile').addEventListener('change', function() {
+  const submitButtonMobile = document.getElementById('submitButtonMobile');
+  submitButtonMobile.disabled = !this.checked;
+});
