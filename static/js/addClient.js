0869fe5458846143b9ef1b25cc4a6ad810c2fbf3
Genrikh Antipov <genrikhantipov@gmail.com>
2024-07-01 01:38:26 +0300

    feat: finish response reaction and redirect to addClient function

--- static/js/addClient.js
+++ static/js/addClient.js
@@ -1,34 +1,35 @@
-document.getElementById('submitButton').addEventListener('click', function() {
-  const form = document.getElementById('form-send');
-  const formData = new FormData(form);
-
-  let phoneNumber = formData.get('phone_number'); 
-  console.log(phoneNumber);
-  phoneNumber = phoneNumber.replace(/[\s()-]/g, '');
-  console.log(phoneNumber);
-  formData.set('phone_number', phoneNumber);
-
-  fetch(form.action, {
-      method: 'POST',
-      headers: {
-          'X-CSRFToken': '{{ csrf_token }}', 
-          'X-Requested-With': 'XMLHttpRequest'
-      },
-      body: formData
-  })
-  .then(response => {
-      if (response.ok) {
-          return response.json(); 
-      } else {
-          return response.json().then(errorData => {
-              throw new Error(errorData.error || 'Произошла ошибка при отправке запроса');
-          });
-      }
-  })
-  .then(data => {
-      alert("Успешно");
-  })
-  .catch(error => {
-      alert('Ошибка: ' + error.message);
-  });
+document.getElementById('submitButton').addEventListener('click', function(event) {
+    event.preventDefault();
+    
+    const form = document.getElementById('form-send');
+    const formData = new FormData(form);
+    
+    let phoneNumber = formData.get('phone_number'); 
+    phoneNumber = phoneNumber.replace(/[\s()-]/g, '');
+    formData.set('phone_number', phoneNumber);
+    
+    fetch(form.action, {
+        method: form.method,
+        headers: {
+            'X-CSRFToken': '{{ csrf_token }}', 
+            'X-Requested-With': 'XMLHttpRequest'
+        },
+        body: formData
+    })
+    .then(response => {
+        if (response.ok) {
+            return response.json(); 
+        } else {
+            return response.json().then(errorData => {
+                throw new Error(errorData.error || 'Произошла ошибка при отправке запроса');
+            });
+        }
+    })
+    .then(data => {
+        alert("Пользователь успешно создан");
+        window.location.href = '/manager/'; 
+    })
+    .catch(error => {
+        alert('Ошибка: ' + error.message);
+    });
 });
